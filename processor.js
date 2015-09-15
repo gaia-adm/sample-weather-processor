b7aa4bfb79aacb30c8bfa53e1e1d866be1682318
Jaroslav Libak <jaroslav.libak@hp.com>
2015-09-15 14:37:50 +0200

    log with proper level and use LOG_LEVEL parameter

--- processor.js
+++ processor.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var logLevelsMap = {
+    'DEBUG': 1, 'INFO': 2, 'WARNING': 3, 'ERROR': 4, 'CRITICAL': 5
+};
+
 var makeSource = require("stream-json");
 var Assembler = require("stream-json/utils/Assembler");
 
@@ -7,14 +11,14 @@ exitOnSignal('SIGTERM');
 
 var params = getProcessParameters();
 
-if (Object.keys(params).length > 0) {
-    // custom metadata keys are prefixed with C_
-    console.error('City: ' + params.C_CITY);
-    console.error('Country: ' + params.C_COUNTRY);
-} else {
+if (Object.keys(params).length === 0) {
     console.log('[]');
     process.exit(0);
 }
+
+// custom metadata keys are prefixed with C_
+log('DEBUG', 'processor.js', 'City: ' + params.C_CITY);
+log('DEBUG', 'processor.js', 'Country: ' + params.C_COUNTRY);
 
 var source = makeSource();
 var assembler = new Assembler();
@@ -37,7 +41,8 @@ process.stdin.setEncoding('utf8');
 process.stdin.pipe(source.input);
 
 /**
- * Processes weather object from <a href="http://api.openweathermap.org/data/2.5/history/city">http://api.openweathermap.org/data/2.5/history/city</a>
+ * Processes weather object from <a
+ * href="http://api.openweathermap.org/data/2.5/history/city">http://api.openweathermap.org/data/2.5/history/city</a>
  */
 function processWeather(weather) {
     // TODO this should ideally be done in stream way - receive individual items instead of the weather object
@@ -70,7 +75,7 @@ function processWeatherItem(item) {
 
 function exitOnSignal(signal) {
     process.on(signal, function() {
-        console.error('Caught ' + signal + ', exiting');
+        log('DEBUG', 'processor.js', 'Caught ' + signal + ', exiting');
         process.exit(1);
     });
 }
@@ -88,3 +93,10 @@ function getProcessParameters() {
     return params;
 }
 
+function log(level, location, message) {
+    var logLevel = logLevelsMap[level];
+    var configuredLogLevel = logLevelsMap[process.env.P_LOG_LEVEL || 'DEBUG'];
+    if (logLevel >=  configuredLogLevel) {
+        console.error(level + ':' + location + ':' + message);
+    }
+}
